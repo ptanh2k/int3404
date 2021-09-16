fedc4d3b716de773aec50e1faac42873a0ce15a5
feelingBlue00 <pta230500x090800@gmail.com>
2021-09-16 17:02:32 +0700

    In week02.py: Complete q_1, q_2

--- week02/week02.py
+++ week02/week02.py
@@ -8,6 +8,7 @@ You should understand the code you write.
 
 import numpy as np
 import cv2
+import sys
 
 
 def q_0(input_file, output_file, delay=1):
@@ -18,8 +19,12 @@ def q_0(input_file, output_file, delay=1):
     :param delay:
     :return:
     """
-    img = cv2.imread(input_file, 1)
-    cv2.imshow('Test img', img)
+    img = cv2.imread(input_file, 1)     # 1_ IMREAD_COLOR
+    
+    if img is None:
+        sys.exit("Could not read the image")
+    
+    cv2.imshow('Apple image', img)
     cv2.waitKey(delay)
 
     cv2.imwrite(output_file, img)
@@ -31,18 +36,60 @@ nếu không, chương trình sẽ tiếp tục chạy ít nhất cho đến khi
 Tham khảo: https://web.archive.org/web/20120122022754/http://opencv.willowgarage.com/wiki/documentation/c/highgui/WaitKey
 """
 
-def q_1():
-    print("Task 1")
+def q_1(input_file):
+    """
+        imread() -> Order: BRG (Blue, Green, Red)
+    """
+    img1 = cv2.imread(input_file)
+    
+    if img1 is None:
+        sys.exit("Could not read the image")
+        
+    (height, width, depth) = img1.shape
+    print("height={}, width={}, depth={}".format(height, width, depth))
+    
+    yCrCbImg = cv2.cvtColor(img1, cv2.COLOR_BGR2YCR_CB)
+    
+    avgY = np.mean(yCrCbImg[:, : , 0])
+    avgCr = np.mean(yCrCbImg[:, :, 1])
+    avgCb = np.mean(yCrCbImg[:, : , 2])
+    
+    print("YCrCb")
+    print("Average of Y: %.2f" % avgY)
+    print("Average of Cr: %.2f" % avgCr)
+    print("Average of Cb: %.2f" % avgCb)
+    
+    rgbImg = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+    
+    avgR = np.mean(rgbImg[:, :, 0])
+    avgG = np.mean(rgbImg[:, :, 1])
+    avgB = np.mean(rgbImg[:, :, 2])
 
+    print("RGB")
+    print("Average of R: %.2f" % avgR)
+    print("Average of G: %.2f" % avgG)
+    print("Average of B: %.2f" % avgB)    
+    
+def q_2(input_file):
+    img2 = cv2.imread(input_file)
+    
+    if img2 is None:
+        sys.exit("Could not read the image")
 
-def q_2():
-    print("Task 2")
-
+    clear_apple = img2[297:471, 363:539]
+    cv2.imshow("Clear apple", clear_apple)
+    cv2.imwrite("./result/clear_apple.png", clear_apple)
+    
+    blurred_apple = img2[39:127, 90:176]
+    cv2.imshow("Blurred apple", blurred_apple)
+    cv2.imwrite("./result/blurred_apple.png", blurred_apple)
+    cv2.waitKey(0)
+    
 
 if __name__ == "__main__":
 
     q_0('./sample_data/apple.png', './result/test_apple.png', 1000)
-#     q_1()
-#     q_2()
+    q_1('./sample_data/chromatic_aberration.png')
+    q_2("./sample_data/apple.png")
 
 
